bb0371755cff89bda1220c1bf3faff8ee88c67f0
dito rafi <diabdusalamrafi@gmail.com>
2024-03-25 02:01:12 +0700

    build app1

--- src/app/components/Company_Overview.tsx
+++ src/app/components/Company_Overview.tsx
@@ -19,7 +19,7 @@ export default function Company_Overview() {
         <div className="mt-6 w-full md:ml-10 md:mt-0">
           <Image
             loading="lazy"
-            src="/Bali.jpg"
+            src="/bali.jpg"
             alt="image"
             width={300}
             height={300}
@@ -28,31 +28,5 @@ export default function Company_Overview() {
         </div>
       </div>
     </section>
-    // <section className=" flex h-screen items-center justify-center bg-gray-100 py-12">
-    //   <div className="container mx-auto items-center justify-items-center md:flex">
-    //     <div className="w-full md:ml-24 ">
-    //       <h1 className="mb-16 text-3xl font-bold text-gray-900 max-md:mt-12 max-md:text-center md:text-4xl">
-    //         Overview
-    //       </h1>
-    //       <p className="text-lg leading-relaxed text-gray-800 md:text-xl">
-    //         Discover our array of curated travel experiences designed to ignite
-    //         your wanderlust. From luxurious retreats on secluded islands to
-    //         adrenaline-fueled expeditions in exotic locales, each journey
-    //         promises unparalleled excitement and relaxation. Start your
-    //         adventure today
-    // //       </p>
-    //     </div>
-    //     <div className="mt-6 w-full md:ml-10 md:mt-0">
-    //       <Image
-    //         loading="lazy"
-    //         src="/Bali.jpg"
-    //         alt="image"
-    //         width={300}
-    //         height={300}
-    //         className="rounded-lg shadow-lg"
-    //       />
-    //     </div>
-    //   </div>
-    // </section>
   );
 }
